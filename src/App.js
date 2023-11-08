91955414fe9bcdb1fc377a9892b4f495ecef1e7d
Joel Abraham Koshy <joelabrahamkoshy@gmail.com>
2023-11-08 07:40:22 +0530

    home

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { HashRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Login from './components/Login';
+import Home from './components/Home';
 
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
       <HashRouter>
         <Navbar />
         <Routes>
+        <Route path="/" element={<Home/>} />
           <Route path="/login" element={<Login/>} />
         </Routes>
       </HashRouter>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,53 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import BannerImage from "../Assets/hospital.jpg";
+import { Link } from "react-router-dom";
+import "../components/home.css";
+import "./home.css";
 
-const Home = () => {
+function Home() {
+  useEffect(() => {
+    function typeText(text, index) {
+      if (index < text.length) {
+        h1Element.textContent += text.charAt(index);
+        index++;
+        setTimeout(function () {
+          typeText(text, index);
+        }, 50); 
+      }
+    }
+
+    const h1Element = document.querySelector(".intro h1");
+
+    
+    const textToType = "Where healing and compassion come together";
+
+   
+    typeText(textToType, 0);
+  }, []);
+
   return (
-    <div>Home</div>
-  )
+    <div className="Home">
+      <div
+        className="headerContainer"
+        style={{ backgroundImage: `url(${BannerImage})` }}
+      >
+        <div className="intro">
+          <h1></h1>
+        </div>
+        <div className="desc">
+          <p>
+            We connect health to happiness. When the dream is to provide a slice
+            of humanity with quality health care, realizing that dream becomes a
+            mission.
+          </p>
+          <br />
+          <Link to="/appointment" className="linkc">Appointment</Link>
+        </div>
+
+        
+      </div>
+    </div>
+  );
 }
 
-export default Home
+export default Home;
