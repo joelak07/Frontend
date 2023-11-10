75f6d9a6f167e9a4eca00fa263bd8bb136887327
Joel Abraham Koshy <joelabrahamkoshy@gmail.com>
2023-11-10 08:25:49 +0530

    Status Page Design

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './status.css';
 import { toast, ToastContainer } from 'react-toastify';
-import {Link, useNavigate} from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Axios from "axios";
 
 const Status = () => {
@@ -10,50 +10,53 @@ const Status = () => {
 
   const [email, setEmail] = useState('');
 
-  const sendOTP = async(e)=>{
+  const sendOTP = async (e) => {
     e.preventDefault();
-    if (email==="") {
+    if (email === "") {
       toast.error("Enter your email");
     }
-    else if(!email.includes('@')) {
+    else if (!email.includes('@')) {
       toast.error("Enter valid email");
     }
     else {
-      const data = {"email":email};
+      const data = { "email": email };
       console.log(data);
       Axios.post("http://localhost:4000/patientOtp/sendOtp", data)
-      .then((res)=>{
-        if (res.status===200) {
-          toast.success("OTP has been sent");
-          navigate("/patient/otp",{state:email});
-        }
-        else {
-          toast.error(res.response.data.error);
-        }
-      })
-      .catch((err)=>{
-        console.log(err);
-        toast.error("error");
-      });
+        .then((res) => {
+          if (res.status === 200) {
+            toast.success("OTP has been sent");
+            navigate("/patient/otp", { state: email });
+          }
+          else {
+            toast.error(res.response.data.error);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+          toast.error("error");
+        });
     }
   }
 
   return (
     <div class="statusContainer">
-      <div class="statusLogBox">
-        <div class="statusFormHeading">
-          Enter your email
+      <div className="statcon">
+        <div className="statdes">
+          <h1>Check <br /> your <br /> appointment <br /> status <br /> here!</h1>
         </div>
-        <form>
-          <div class="statusFormInput">
-            <label htmlFor='email'>Email</label>
-            <input type='email' name='email' onChange={(e)=>{setEmail(e.target.value)}} placeholder='Enter your email' />
-          </div>
-          <button class="btn" onClick={sendOTP}>Send OTP</button>
-          <p>Don't have an appointment <Link to="/appointment">Book now</Link></p>
-        </form>
+        <div class="statusLogBox">
+          <form>
+            <div class="statusFormInput">
+              <label htmlFor='email'>Email</label>
+              <input type='email' name='email' onChange={(e) => { setEmail(e.target.value) }} placeholder='Enter your email' />
+            </div>
+            <button class="stbtn" onClick={sendOTP}>Send OTP</button>
+            <p>Don't have an appointment ? <Link to="/appointment" className='links'>Book now</Link></p>
+          </form>
+          <ToastContainer />
+        </div>
       </div>
-      <ToastContainer />
+
     </div>
   )
 }
