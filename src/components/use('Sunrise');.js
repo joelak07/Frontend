2c90f6a879d40d2d85eb6e83fa865e22e15364fc
ShankarJyothish8 <shankarjyothish8@gmail.com>
2023-11-10 19:26:08 +0400

    Create use('Sunrise');.js

---
+++ src/components/use('Sunrise');.js
@@ -0,0 +1,9 @@
+use('Sunrise');
+db.patient.insertOne({
+  "patientId": 7,
+  "patientName": "Shankar Jyothish",
+  "email": "shankarjyothish8@gmail.com",
+  "phoneNo": "4967672697",
+  "address": "ABC",
+  "dob": new Date("2003-06-17T00:00:00Z")
+});
