145eb1f2faae838db0d1c0e26654dac45050210b
ShankarJyothish8 <shankarjyothish8@gmail.com>
2023-11-15 15:12:45 +0400

    Confirmed page

--- src/App.js
+++ src/App.js
@@ -1,32 +1,32 @@
-import { HashRouter, Routes, Route } from 'react-router-dom';
-import './App.css';
-import Navbar from './components/Navbar';
-import Login from './components/Login';
-import Appointment from './components/Appointment';
-import Home from './components/Home';
-import Footer from './components/Footjer';
-import Status from './components/Status';
-import Error from './components/Error';
-import Otp from './components/Otp';
-import VerifiedStatus from './components/VerifiedStatus';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+import { HashRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Login from "./components/Login";
+import Appointment from "./components/Appointment";
+import Home from "./components/Home";
+import Footer from "./components/Footjer";
+import Status from "./components/Status";
+import Error from "./components/Error";
+import Otp from "./components/Otp";
+import VerifiedStatus from "./components/VerifiedStatus";
+import Confirmed from "./components/Confirmed";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
-
   return (
     <div>
       <HashRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/login" element={<Login/>} />
-          <Route path="/status" element={ <Status /> } />
-          <Route path="/appointment" element={ <Appointment/>} />
-          <Route path="/*" element={ <Error/>} />
-          <Route path="/patient/otp" element={ <Otp /> } />
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/status" element={<Status />} />
+          <Route path="/appointment" element={<Appointment />} />
+          <Route path="/*" element={<Error />} />
+          <Route path="/patient/otp" element={<Otp />} />
           <Route path="/verifiedStatus" element={<VerifiedStatus />} />
+          <Route path="/confirmed" element={<Confirmed />} />
         </Routes>
         <Footer />
       </HashRouter>
