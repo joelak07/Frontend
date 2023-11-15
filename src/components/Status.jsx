23dc41c4ade652a31499850a4c964ad321006a85
Joel Abraham Koshy <joelabrahamkoshy@gmail.com>
2023-11-15 07:46:12 +0530

    spinner added

--- src/components/Status.jsx
+++ src/components/Status.jsx
@@ -5,64 +5,63 @@ import { Link, useNavigate } from 'react-router-dom';
 import Axios from "axios";
 
 const Status = () => {
-
   const navigate = useNavigate();
-
   const [email, setEmail] = useState('');
+  const [loading, setLoading] = useState(false);
 
-  useEffect(()=>{
+  useEffect(() => {
     localStorage.removeItem("patientdbtoken");
-  },[]);
+  }, []);
 
   const sendOTP = async (e) => {
     e.preventDefault();
     if (email === "") {
       toast.error("Enter your email");
-    }
-    else if (!email.includes('@')) {
-      toast.error("Enter valid email");
-    }
-    else {
+    } else if (!email.includes('@')) {
+      toast.error("Enter a valid email");
+    } else {
+      setLoading(true); 
       const data = { "email": email };
       console.log(data);
-      Axios.post("http://localhost:4000/patientOtp/sendOtp", data)
-        .then((res) => {
-          if (res.status === 200) {
-            toast.success("OTP has been sent");
-            navigate("/patient/otp", { state: email });
-          }
-          else {
-            toast.error(res.response.data.error);
-          }
-        })
-        .catch((err) => {
-          console.log(err);
-          toast.error("error");
-        });
+      try {
+        const res = await Axios.post("http://localhost:4000/patientOtp/sendOtp", data);
+        if (res.status === 200) {
+          toast.success("OTP has been sent");
+          navigate("/patient/otp", { state: email });
+        } else {
+          toast.error(res.response.data.error);
+        }
+      } catch (err) {
+        console.log(err);
+        toast.error("Error");
+      } finally {
+        setLoading(false);
+      }
     }
-  }
+  };
 
   return (
-    <div class="statusContainer">
+    <div className="statusContainer">
       <div className="statcon">
         <div className="statdes">
           <h1>Check <br /> your <br /> appointment <br /> status <br /> here!</h1>
         </div>
-        <div class="statusLogBox">
+        <div className="statusLogBox">
           <form>
-            <div class="statusFormInput">
+            <div className="statusFormInput">
               <label htmlFor='email'>Email</label>
               <input type='email' name='email' onChange={(e) => { setEmail(e.target.value) }} placeholder='Enter your email' />
             </div>
-            <button class="stbtn" onClick={sendOTP}>Send OTP</button>
-            <br/>
-            <br/> 
+            <button className="stbtn" onClick={sendOTP} disabled={loading}>
+              {loading ? <div className="spinner"></div> : 'Send OTP'}
+            </button>
+            <br />
+            <br />
             <p>Don't have an appointment ? <Link to="/appointment" className='linkst'>Book now</Link></p>
           </form>
           <ToastContainer />
         </div>
       </div>
-
     </div>
   )
 }
