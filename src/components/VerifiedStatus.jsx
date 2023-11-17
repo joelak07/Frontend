cebac8f505c74dfb18b6b219ab2f00d80155ef5e
ShankarJyothish8 <shankarjyothish8@gmail.com>
2023-11-17 12:05:48 +0400

    appointmen update

--- src/components/VerifiedStatus.jsx
+++ src/components/VerifiedStatus.jsx
@@ -2,46 +2,48 @@ import { useEffect, useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import Axios from "axios";
 import PatientObj from "./PatientObj";
+import "./verifiedStatus.css";
 
 function VerifiedStatus() {
-    const navigate = useNavigate();
-    const location = useLocation();
-    const [arr, setArr] = useState([]);
-    useEffect(() => {
-        const patientValid = () => {
-            let token = localStorage.getItem("patientdbtoken");
-            if (token) {
-                console.log("User valid");
-                console.log(location.state);
-                Axios.get("http://localhost:4000/appointment/getAppointment", {params:{email:location.state}})
-                .then((res)=>{
-                    if(res.status===200) {
-                        setArr(res.data);
-                    }
-                    else {
-                        Promise.reject();
-                    }
-                })
-                .catch((err)=>alert(err));
-            } else {
-                console.log("invalid details");
-                navigate("*");
-            }
-        }
-        patientValid();
-    }, [navigate]);
-    const ListItems = () => {
-        return arr.map((val,ind)=>{
-            return <PatientObj key={val._id} obj={val} />
+  const navigate = useNavigate();
+  const location = useLocation();
+  const [arr, setArr] = useState([]);
+  useEffect(() => {
+    const patientValid = () => {
+      let token = localStorage.getItem("patientdbtoken");
+      if (token) {
+        console.log("User valid");
+        console.log(location.state);
+        Axios.get("http://localhost:4000/appointment/getAppointment", {
+          params: { email: location.state },
         })
-    }
-    return (
-        <div>
-            <div>
-                <div>{ListItems()}</div>
-            </div>
-        </div>
-    )
+          .then((res) => {
+            if (res.status === 200) {
+              setArr(res.data);
+            } else {
+              Promise.reject();
+            }
+          })
+          .catch((err) => alert(err));
+      } else {
+        console.log("invalid details");
+        navigate("*");
+      }
+    };
+    patientValid();
+  }, [navigate]);
+  const ListItems = () => {
+    return arr.map((val, ind) => {
+      return <PatientObj key={val._id} obj={val} />;
+    });
+  };
+  return (
+    <div className="verifiedcont">
+      <div className="verifiedcon">
+        <div>{ListItems()}</div>
+      </div>
+    </div>
+  );
 }
 
 export default VerifiedStatus;
