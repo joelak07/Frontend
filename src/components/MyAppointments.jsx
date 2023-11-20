9d9b7b74a0bdce8b6272a097d18238688a52cbad
NaveedHafeez004 <144110314+NaveedHafeez004@users.noreply.github.com>
2023-11-20 16:22:34 +0530

    my appointment fetch fixed

--- src/components/MyAppointments.jsx
+++ src/components/MyAppointments.jsx
@@ -2,12 +2,24 @@ import React, { useState, useEffect } from 'react';
 import Axios from 'axios';
 import DocNav from './DocNav';
 import AppointmentObj from './AppointmentObj';
-import "./myappointments.css";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import './myappointments.css';
+import { useNavigate } from 'react-router-dom';
 
 const MyAppointments = () => {
   const [appointments, setAppointments] = useState([]);
   const [selectedDate, setSelectedDate] = useState(new Date().toISOString().split('T')[0]);
   const [patientName, setPatientName] = useState('');
+  const [arr, setArr] = useState([]);
+  const [currentTime, setCurrentTime] = useState('');
+  const navigate = useNavigate();
+  const [docName, setDocName] = useState(null);
+  const [greeting, setGreeting] = useState('');
+  const [upcomingAppointments, setUpcomingAppointments] = useState([]);
+  const [ongoingAppointments, setOngoingAppointments] = useState([]);
+
+  const activeAppointments = arr.filter(appointment => !appointment.isCompleted);
 
   const decodeToken = (token) => {
     const base64Url = token.split('.')[1];
@@ -16,33 +28,205 @@ const MyAppointments = () => {
     return decoded;
   };
 
-  const fetchAppointments = async () => {
+  const fetchData = async () => {
     try {
-      console.log('Fetching appointments for date:', selectedDate);
-
       const token = localStorage.getItem('doctordbtoken');
       if (token) {
         const decodedToken = decodeToken(token);
-        if (decodedToken && decodedToken.doctorId) {
-          const response = await Axios.get(`http://localhost:4000/appointment/getAppointmentForDoctorByDate/${decodedToken.doctorId}/${selectedDate}/${patientName}`);
+        if (decodedToken && decodedToken.empId) {
+          const docData = await Axios.get("http://localhost:4000/doctor/getDoctor", {
+            params: { doctorId: decodedToken.empId }
+          });
+          if (docData.status === 200) {
+            setDocName(docData.data[0].doctorName);
+          }
 
-          console.log('Response:', response.data);
+          // Fetch appointments for the specified date
+          const response = await Axios.get("http://localhost:4000/appointment/getAppointmentForDoctorByDate", {
+            params: { doctorId: decodedToken.empId, date: selectedDate }
+          });
 
           if (response.status === 200) {
-            setAppointments(response.data);
+            const allAppointments = response.data;
+
+            // Filter upcoming and ongoing appointments
+            const upcoming = allAppointments.filter(appointment => appointment.appointmentDate > selectedDate);
+            const ongoing = allAppointments.filter(appointment => appointment.appointmentDate === selectedDate);
+
+            setUpcomingAppointments(upcoming);
+            setOngoingAppointments(ongoing);
           } else {
             console.error("Failed to fetch data");
           }
         }
+      } else {
+        navigate("*");
       }
     } catch (error) {
-      console.error('Error fetching appointments:', error);
+      console.error(error);
+      alert("An error occurred while fetching data");
     }
   };
 
+  const fetchDataPatient = async () => {
+    try {
+      const token = localStorage.getItem('doctordbtoken');
+      if (token) {
+        const decodedToken = decodeToken(token);
+        if (decodedToken && decodedToken.empId) {
+          const docData = await Axios.get("http://localhost:4000/doctor/getDoctor", {
+            params: { doctorId: decodedToken.empId }
+          });
+          if (docData.status === 200) {
+            setDocName(docData.data[0].doctorName);
+          }
+  
+          // Fetch appointments for the specified date and patient name
+          const response = await Axios.get("http://localhost:4000/appointment/getAppointmentForDoctorByDateAndPatient", {
+            params: { doctorId: decodedToken.empId, date: selectedDate, patientName }
+          });
+  
+          if (response.status === 200) {
+            const allAppointments = response.data;
+  
+            // Filter upcoming and ongoing appointments
+            const today = new Date().toISOString().split('T')[0];
+            const upcoming = allAppointments.filter(appointment => new Date(appointment.appointmentDate) > new Date(today));
+            const ongoing = allAppointments.filter(appointment => {
+              const appointmentDate = new Date(appointment.appointmentDate);
+              const todayDate = new Date(today);
+              
+              return (
+                appointmentDate.getFullYear() === todayDate.getFullYear() &&
+                appointmentDate.getMonth() === todayDate.getMonth() &&
+                appointmentDate.getDate() === todayDate.getDate()
+              );
+            });
+  
+            setUpcomingAppointments(upcoming);
+            setOngoingAppointments(ongoing);
+          } else {
+            console.error("Failed to fetch data");
+          }
+        }
+      } else {
+        navigate("*");
+      }
+    } catch (error) {
+      console.error(error);
+      alert("An error occurred while fetching data");
+    }
+  };
+  
+
+const fetchAppointmentsByPatientName = async () => {
+  try {
+    const token = localStorage.getItem('doctordbtoken');
+    if (token) {
+      const decodedToken = decodeToken(token);
+      if (decodedToken && decodedToken.empId) {
+        const docData = await Axios.get("http://localhost:4000/doctor/getDoctor", {
+          params: { doctorId: decodedToken.empId }
+        });
+        if (docData.status === 200) {
+          setDocName(docData.data[0].doctorName);
+        }
+
+        // Fetch appointments for the specified date and patient name
+        const response = await Axios.get("http://localhost:4000/appointment/getAppointmentForDoctorByPatientName", {
+          params: { doctorId: decodedToken.empId,patientName }
+        });
+
+        if (response.status === 200) {
+          const allAppointments = response.data;
+
+          // Filter upcoming and ongoing appointments
+          const today = new Date().toISOString().split('T')[0];
+          const upcoming = allAppointments.filter(appointment => new Date(appointment.appointmentDate) > new Date(today));
+          const ongoing = allAppointments.filter(appointment => {
+            const appointmentDate = new Date(appointment.appointmentDate);
+            const todayDate = new Date(today);
+            
+            return (
+              appointmentDate.getFullYear() === todayDate.getFullYear() &&
+              appointmentDate.getMonth() === todayDate.getMonth() &&
+              appointmentDate.getDate() === todayDate.getDate()
+            );
+          });
+
+          setUpcomingAppointments(upcoming);
+          setOngoingAppointments(ongoing);
+        } else {
+          console.error("Failed to fetch data");
+        }
+      }
+    } else {
+      navigate("*");
+    }
+  } catch (error) {
+    console.error(error);
+    alert("An error occurred while fetching data");
+  }
+};
+
+
+  const fetchDataAll = async () => {
+    try {
+      const token = localStorage.getItem('doctordbtoken');
+  
+      if (token) {
+        const decodedToken = decodeToken(token);
+  
+        if (decodedToken && decodedToken.empId) {
+          const docData = await Axios.get("http://localhost:4000/doctor/getDoctor", {
+            params: { doctorId: decodedToken.empId }
+          });
+  
+          if (docData.status === 200) {
+            setDocName(docData.data[0].doctorName);
+          }
+  
+          // Fetch all appointments for the logged-in doctor using the patientName route
+          const response = await Axios.get("http://localhost:4000/appointment/getAppointmentForDoctorByPatientName", {
+            params: { doctorId: decodedToken.empId, patientName: null }
+          });
+  
+          if (response.status === 200) {
+            const allAppointments = response.data;
+  
+            // Filter upcoming and ongoing appointments
+            const today = new Date().toISOString().split('T')[0];
+            const upcoming = allAppointments.filter(appointment => new Date(appointment.appointmentDate) > new Date(today));
+            const ongoing = allAppointments.filter(appointment => {
+              const appointmentDate = new Date(appointment.appointmentDate);
+              const todayDate = new Date(today);
+            
+              return (
+                appointmentDate.getFullYear() === todayDate.getFullYear() &&
+                appointmentDate.getMonth() === todayDate.getMonth() &&
+                appointmentDate.getDate() === todayDate.getDate()
+              );
+            });
+              
+            setUpcomingAppointments(upcoming);
+            setOngoingAppointments(ongoing);
+          } else {
+            console.error('Failed to fetch data');
+          }
+        }
+      } else {
+        navigate("*");
+      }
+    } catch (error) {
+      console.error('Error in fetchDataAll:', error);
+      alert('An error occurred while fetching data');
+    }
+  };
+  
+
   useEffect(() => {
-    fetchAppointments();
-  }, [selectedDate, patientName]);
+    fetchDataAll();
+  }, []);
 
   const handleDateChange = (event) => {
     const date = event.target.value;
@@ -52,6 +236,27 @@ const MyAppointments = () => {
   const handlePatientNameChange = (event) => {
     const name = event.target.value;
     setPatientName(name);
+  };
+
+  const handleSearch = () => {
+    if (selectedDate.trim() !== "" && patientName.trim() !== "") {
+      // If both date and patient name are provided, call fetchDataPatient
+      console.log("Fetching data by both date and patient name");
+      fetchDataPatient();
+    } else if (selectedDate.trim() !== "") {
+      // If only date is provided, call fetchData
+      console.log("Fetching data by date");
+      fetchData();
+    } else if (patientName.trim() !== "") {
+      // If only patient name is provided, call fetchAppointmentsByPatientName
+      console.log("Fetching data by patient name");
+      fetchAppointmentsByPatientName();
+    } else {
+      // Handle the case when neither date nor patient name is provided
+      // Display all appointments
+      console.log("Fetching all data");
+      fetchDataAll();
+    }
   };
 
   return (
@@ -80,24 +285,35 @@ const MyAppointments = () => {
                 onChange={handlePatientNameChange}
               />
             </div>
-            <button onClick={fetchAppointments} className='myappbutt'>Search</button>
+            <button onClick={handleSearch} className='myappbutt'>Search</button>
           </div>
-        </div>
+        
+          <div className="appsearchres-container">
+            <div className="appsearchres">
+              <h3>Upcoming Appointments</h3>
+              <ul>
+                {upcomingAppointments.map((appointment) => (
+                  <AppointmentObj key={appointment._id} obj={appointment} />
+                ))}
+              </ul>
+            </div>
+            <div className="appsearchres">
+              <h3>Ongoing Appointments</h3>
+              <ul>
+                {ongoingAppointments.map((appointment) => (
+                  <AppointmentObj key={appointment._id} obj={appointment} />
+                ))}
+              </ul>
+            </div>
+          </div>
 
-        {appointments.length > 0 ? (
-          <div className="appsearchres">
-            <ul>
-              {appointments.map((appointment) => (
-                <AppointmentObj key={appointment._id} obj={appointment} />
-              ))}
-            </ul>
-          </div>
-        ) : (
-          <p>No appointments found</p>
-        )}
+          {upcomingAppointments.length === 0 && ongoingAppointments.length === 0 && (
+            <p>No appointments found</p>
+          )}
+        </div>
       </div>
+      <ToastContainer />
     </div>
   );
 };
-
 export default MyAppointments;
